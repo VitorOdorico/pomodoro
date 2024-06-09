fffc0d5de5afa78e264630e05056e874cd801b0d
Vitor Emanuel Odorico Neto Costa <VCosta@solucaonetwork.com>
2024-06-09 15:59:15 -0300

    add to switch case to strings

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
 const card = document.querySelector('.app__card')
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+
 
 focoBtn.addEventListener('click', ()=>{
    alterarContexto('foco');
@@ -19,8 +21,30 @@ longoBtn.addEventListener('click', ()=>{
 })
 
 function alterarContexto(contexto){
-    html.setAttribute('data-contexto', contexto),
-    banner.setAttribute('src', `/imagens/${contexto}.png`)
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `/imagens/${contexto}.png`);
+    switch(contexto){
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+        break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+        break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `
+        break;
+        default:
+            break;
+    }
 }
 
 
